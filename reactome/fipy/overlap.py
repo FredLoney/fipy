7f519fd6ea16c812ae1abb2c1e2ee3649f9fa1f4
FredLoney <loneyf@ohsu.edu>
2018-05-09 13:14:41 -0700

    Add limit_overlap method.

--- reactome/fipy/overlap.py
+++ reactome/fipy/overlap.py
@@ -2,6 +2,7 @@ from functools import reduce
 import pandas as pd
 from scipy.stats import hypergeom
 from statsmodels.sandbox.stats.multicomp import multipletests
+from IPython.display import HTML
 from .constants import BACKGROUND_GENE_CNT
 
 
@@ -22,9 +23,11 @@ def analyse(clusters, n=None):
     # Make the overlap multi-index.
     index_names = [cluster.name for cluster in clusters]
     index_values = [cluster.index.values for cluster in clusters]
-    multi_index = pd.MultiIndex.from_product(index_values, names=index_names)
+    multi_index = pd.MultiIndex.from_product(index_values,
+                                             names=index_names)
     # Take the pair-wise intersections.
-    genesets = [[set(gs) for gs in ds.values] for ds in clusters]
+    genesets = [[set(gs) for gs in cluster.values]
+                for cluster in clusters]
     data = [gs1.intersection(gs2)
             for gs1 in genesets[0]
             for gs2 in genesets[1]]
@@ -51,10 +54,27 @@ def analyse(clusters, n=None):
     return shared.assign(**cols)
 
 
-def unshared(shared, clusters):
+def limit_overlap(overlaps, column, cutoff):
     """
-    :param shared: the overlap data frame
-    :param clusters: the cluster data series
+    Restricts the given overlaps data frame to those module
+    pairs whose FDR is no greater than the given cut-off.
+    
+    :param overlaps: the overlaps data frame
+    :param column: the overlaps column name (`FDR` or `p-value`)
+    :param cutoff: the threshold value
+    """
+    return overlaps[overlaps[column].le(cutoff)]
+
+
+def unshared(clusters, shared):
+    """
+    Finds each cluster modules subset which is not
+    in the given shared overlap data frame. The input
+    clusters must be in the same order as the analyse
+    *clusters* argument.
+
+    :param clusters: the list of cluster data series
+    :param shared: the shared overlap data frame
     :return: the filtered clusters
     """
     return [_difference(cluster, shared.index, i)
@@ -84,47 +104,38 @@ def _index_level_values(multi_index, level):
     return multi_index.get_level_values(level).unique()
 
 
-def print_overlap(overlap_df, cutoff=None, format='html'):
+def print_overlap(overlaps, qualifier=None, format='html'):
     """
-    Represents the given overlap data frame as a data frame
+    Represents the given overlap data frame as a string
     suitable for printing.
 
-    :param overlap_df: the raw overlap data frame
-    :option cutoff: the FDR cut-off value
+    :param overlaps: the overlap data frame
+    :option qualifier: the optional qualifier to print
+        in the HTML-formatted title
     :option format: 'html' or 'text' (default is html)
-    :return: a data frame suitable for printing
+    :return: the printable string
     """
-    # Filter the overlap FDR. Set this to a lower value to
-    # Restrict the table size.
-    if cutoff:
-        filtered_df = overlap_df[overlap_df.FDR.le(cutoff)]
-    else:
-        filtered_df = overlap_df
-    rename_opts = {name: "%s Module" % name
-                   for name in filtered_df.index.names}
-    flat_df = filtered_df.reset_index().rename(rename_opts, axis=1)
+    flat = overlaps.reset_index()
 
-    columns = [col for col in flat_df.columns if not col.endswith("Genes")]
+    columns = [col for col in flat.columns if not col.endswith("Genes")]
     cohort_col_grps = [[col for col in columns if col.startswith(name)]
-                       for name in filtered_df.index.names]
+                       for name in overlaps.index.names]
     cohort_cols = reduce(lambda x,y: x + y, cohort_col_grps)
     non_cohort_cols = [col for col in columns if col not in cohort_cols]
     ordered_cols = cohort_cols + non_cohort_cols
-    printable_df = flat_df.loc[:, ordered_cols]
+    printable = flat.loc[:, ordered_cols]
     if format == 'text':
-        print(printable_df.to_string(index=False))
+        return printable.to_string(index=False)
     elif format == 'html':
         start = '<h4>Module Overlap'
-        if cutoff:
+        if qualifier:
             style = 'font-size:normal;font-weight:normal;'
-            middle = "<span style=%s> (FDR <= %s)</span>" % (style, cutoff)
+            middle = "<span style=%s> (%s)</span>" % (style, qualifier)
         else:
             middle = ''
         end = '</h4>'
         heading = start + middle + end
-        hdg = HTML(heading)
-        table = HTML(printable_df.to_html(index=False))
-        return hdg + table
+        return HTML(heading + printable.to_html(index=False))
     else:
         raise ValueError("Unrecognized overlap print format: %s" % format)
 
